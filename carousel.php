a18c32cda9d3334a03abe1275fb9cadcc841528d
Minu <minuapple@gmail.com>
2015-01-14 09:37:26 +0530

    updated php files

---
+++ carousel.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css">
+	<style type="text/css">
+		
+		@media (min-width:768px) {
+			
+    		#im1{
+      			width: 422px;
+      			height: 250px;
+		    }   
+ 		}
+	 	@media(max-width: 768px){
+ 			#im1{
+ 				width: 200px;
+ 				position: relative;
+ 				top: 10px;
+ 				height:150px;
+ 			}	
+ 			
+ 		}
+	</style>
+</head>
+<body>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  								<div class="carousel-inner" role="listbox">
+    								<div class="item active">
+      									<img src="logomak.png" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+    								<div class="item">
+      									<img src="pop2.jpg" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+    								<div class="item">
+      									<img src="pop3.jpg" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+    								<div class="item">
+      									<img src="pop4.jpg" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+    								<div class="item">
+      									<img src="pop5.jpg" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+    								<div class="item">
+      									<img src="pop6.jpg" id="im1" >
+      									<div class="carousel-caption"></div>
+    								</div>
+  								</div>
+  								<a id="p" class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    								<span class="sr-only">Previous</span>
+  								</a>
+  								<a id="n" class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    								<span class="sr-only">Next</span>
+  								</a>
+							</div>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
--- homeonly.php
+++ homeonly.php
@@ -120,9 +120,7 @@
     		top: 10px;
     		left: 25px;
    		}
-   		#im0{
-    		padding-left: 20px;
-    	}
+   		
     	#d{
     		text-align: center;
     	}
@@ -203,64 +201,18 @@
 				<div class="row">
 					<div class="col-xs-3 hidden-sm hidden-md hidden-lg"></div>
 					<div class="hidden-xs col-sm-3 col-md-3 col-lg-3" id="d">
-<p id="p1">
-<b>Our Clients:</b></p>
-<img src="tiger.jpg" id="im0" width="100%" class="img-responsive"></div><div class="col-xs-12 col-sm-5 col-md-5 col-lg-5" id="d3"><br>
+<?php include("ourclients.php");?></div><div class="col-xs-12 col-sm-5 col-md-5 col-lg-5" id="d3"><br>
 <p id="p2">Established in the year 1997, we were <b>the first to manufacture Plaster Of Paris Bandages B.P (P.O.P Bandages) in Gujarat</b> - one of the most industrialized states in India.</p>
 <p id="p2">The entire technology involved in manufacturing has been conceptualized, designed and made by us which notonly produces economic versality. Recognizing this achievement the Government of Gujarat, as a special case, conferred the authority of manufacturing chemist on Shri. Bhupendra Makim, one of our partners, who is an engineer by qualification.</p>
 <br>
 <p id="p2"><b>Currently, we supply our bandages under thebrand name of MEDICAST to several individual Doctors, Hospitals, Several Government agencies (through tenders) as well as merchant exporters. We are also capable of manufacturing customsized P.O.P Bandages under custom brand names (pre-dominantly used for export).</b></p>
 </div><br>
 <div class="col-xs-6 hidden-sm hidden-md hidden-lg">
-<p id="p1">
-<b>Our Clients:</b></p>
-<img src="tiger.jpg" width="100%" class="img-responsive"></div>
+<?php include("ourclients.php");?></div>
 <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4" id="d4">
 	<br>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
- 
-  
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="logomak.png" id="im1" >
-      <div class="carousel-caption">
-       </div>
-    </div>
-    <div class="item">
-      <img src="pop2.jpg" id="im1" >
-      <div class="carousel-caption">
-        </div>
-    </div>
-    <div class="item">
-      <img src="pop3.jpg" id="im1" >
-      <div class="carousel-caption">
-        </div>
-    </div>
-    <div class="item">
-      <img src="pop4.jpg" id="im1" >
-      <div class="carousel-caption">
-        </div>
-    </div>
-    <div class="item">
-      <img src="pop5.jpg" id="im1" >
-      <div class="carousel-caption">
-        </div>
-    </div>
-    <div class="item">
-      <img src="pop6.jpg" id="im1" >
-      <div class="carousel-caption">
-        </div>
-    </div>
-  </div>
-  <a id="p" class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a id="n" class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div></div></div></div>
+	<?php include("carousel.php");?>
+</div></div></div>
 	</div></div>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
